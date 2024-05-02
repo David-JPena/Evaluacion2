b9dfab9083a855eed9035e4b0153b8f0a1338044
david <jimeenzpenadavid978@gmail.com>
2024-05-02 14:28:30 -0500

    Agregar archivos de proyecto.

---
+++ Evaluacion2/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Evaluacion2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbEmpleado = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpInicio = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpFin = new System.Windows.Forms.DateTimePicker();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnMTodo = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.totalResultado = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.totalResultado);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.btnMTodo);
+            this.groupBox1.Controls.Add(this.btnMostrar);
+            this.groupBox1.Controls.Add(this.dtpFin);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.dtpInicio);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbEmpleado);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(890, 304);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Consulta Ordenes por empleado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Empleado";
+            // 
+            // cmbEmpleado
+            // 
+            this.cmbEmpleado.FormattingEnabled = true;
+            this.cmbEmpleado.Location = new System.Drawing.Point(78, 31);
+            this.cmbEmpleado.Name = "cmbEmpleado";
+            this.cmbEmpleado.Size = new System.Drawing.Size(146, 21);
+            this.cmbEmpleado.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(236, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Desde el";
+            // 
+            // dtpInicio
+            // 
+            this.dtpInicio.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpInicio.Location = new System.Drawing.Point(299, 32);
+            this.dtpInicio.Name = "dtpInicio";
+            this.dtpInicio.Size = new System.Drawing.Size(84, 20);
+            this.dtpInicio.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(389, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hasta el ";
+            // 
+            // dtpFin
+            // 
+            this.dtpFin.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFin.Location = new System.Drawing.Point(453, 32);
+            this.dtpFin.Name = "dtpFin";
+            this.dtpFin.Size = new System.Drawing.Size(84, 20);
+            this.dtpFin.TabIndex = 5;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnMostrar.Location = new System.Drawing.Point(658, 32);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(87, 28);
+            this.btnMostrar.TabIndex = 6;
+            this.btnMostrar.Text = "Mostra ";
+            this.btnMostrar.UseVisualStyleBackColor = false;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnMTodo
+            // 
+            this.btnMTodo.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnMTodo.Location = new System.Drawing.Point(781, 31);
+            this.btnMTodo.Name = "btnMTodo";
+            this.btnMTodo.Size = new System.Drawing.Size(89, 29);
+            this.btnMTodo.TabIndex = 7;
+            this.btnMTodo.Text = "Mostra  Todo";
+            this.btnMTodo.UseVisualStyleBackColor = false;
+            this.btnMTodo.Click += new System.EventHandler(this.btnMTodo_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(862, 193);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // totalResultado
+            // 
+            this.totalResultado.BackColor = System.Drawing.Color.MediumPurple;
+            this.totalResultado.Location = new System.Drawing.Point(781, 278);
+            this.totalResultado.Name = "totalResultado";
+            this.totalResultado.Size = new System.Drawing.Size(100, 20);
+            this.totalResultado.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox totalResultado;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnMTodo;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.DateTimePicker dtpFin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpInicio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbEmpleado;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Evaluacion2/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluacion2
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = Properties.Settings.Default.cnx;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarEmpleados();
+            CargarPedidos();
+        }
+
+        private void CargarPedidos()
+        {
+            // Llamar al procedimiento almacenado para obtener todos los pedidos
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("pa_listaOrdenesempleado", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dtPedidos = new DataTable(); // Crear DataTable para almacenar los pedidos
+                adapter.Fill(dtPedidos);
+
+                // Mostrar todos los pedidos en el DataGridView
+                dataGridView1.DataSource = dtPedidos;
+            }
+        }
+
+        private void CargarEmpleados()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SELECT DISTINCT ApeEmpleado FROM dbo.EMPLEADO", connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        cmbEmpleado.Items.Add(reader["ApeEmpleado"].ToString());
+                    }
+                    reader.Close();
+                }
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            // Obtener el ID del empleado seleccionado en el ComboBox
+            string nombreCompleto = cmbEmpleado.SelectedItem.ToString();
+
+            // Dividir el nombre completo en apellido y nombre
+            string[] nombreParts = nombreCompleto.Split(',');
+            string apellido = nombreParts[0].Trim(); // Obtener el apellido
+
+            // Obtener las fechas de inicio y fin
+            DateTime fechaInicio, fechaFin;
+            if (!DateTime.TryParse(dtpInicio.Text, out fechaInicio) || !DateTime.TryParse(dtpFin.Text, out fechaFin))
+            {
+                MessageBox.Show("Por favor, ingrese fechas válidas.");
+                return;
+            }
+
+            // Llamar al procedimiento almacenado para obtener los pedidos por empleado y fechas
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("pa_listaOrdenesPorEmpleado", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@ApeEmpleado", apellido); // Pasar el apellido como parámetro
+                command.Parameters.AddWithValue("@f1", fechaInicio);
+                command.Parameters.AddWithValue("@f2", fechaFin);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dtPedidos = new DataTable();
+                adapter.Fill(dtPedidos);
+
+                // Mostrar los resultados en el DataGridView
+                dataGridView1.DataSource = dtPedidos;
+
+                // Cambiar el nombre de las columnas del DataGridView
+                dataGridView1.Columns["IdPedido"].HeaderText = "ID Pedido";
+                dataGridView1.Columns["FechaPedido"].HeaderText = "Fecha Pedido";
+                dataGridView1.Columns["NombreCia"].HeaderText = "Nombre de la Compañía";
+                dataGridView1.Columns["Direccion"].HeaderText = "Dirección";
+                dataGridView1.Columns["CiudadDestinatario"].HeaderText = "Ciudad Destinatario";
+                dataGridView1.Columns["PaisDestinatario"].HeaderText = "País Destinatario";
+                dataGridView1.Columns["ApeEmpleado"].HeaderText = "Apellido del Empleado";
+
+                dataGridView1.Columns["Total"].HeaderText = "Total";
+
+                // Mostrar la cantidad de órdenes en el TextBox
+                totalResultado.Text = dtPedidos.Rows.Count.ToString() + " registros";
+            }
+        }
+
+        private void btnMTodo_Click(object sender, EventArgs e)
+        {
+             using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        SqlCommand command = new SqlCommand("pa_listaOrdenesempleado", connection);
+        command.CommandType = CommandType.StoredProcedure;
+
+        SqlDataAdapter adapter = new SqlDataAdapter(command);
+        DataTable dtPedidos = new DataTable(); // Crear DataTable para almacenar los pedidos
+        adapter.Fill(dtPedidos);
+
+        // Mostrar todos los pedidos en el DataGridView
+        dataGridView1.DataSource = dtPedidos;
+                // Limpiar el TextBox
+                totalResultado.Text = "";
+            }
+        }
+    }
+}
+
+
